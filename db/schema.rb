272d6a541196ebbcc4621bf1138f2bc1739bafb7
hansvinvenetheethan <hansvin18@gmail.com>
2021-11-16 12:38:34 +0400

    blah

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_141048) do
+ActiveRecord::Schema.define(version: 2021_11_15_170207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_141048) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["parents_id_id"], name: "index_students_on_parents_id_id"
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
@@ -28,7 +29,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_141048) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
 
   create_table "users", force: :cascade do |t|
