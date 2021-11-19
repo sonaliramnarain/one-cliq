ea02087ac1bf575ab4de757ee3a2d63b09caf4ca
inna goordoyal <innagoordoyal@gmail.com>
2021-11-19 15:36:29 +0400

    model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
- grade_7 = Classroom.create( level: "Grade 7" )
-
- grade_8 = Classroom.create(level: "Grade 8" )
-
- grade_9  = Classroom.create(level: "Grade 9" )
-
- grade_10 = Classroom.create(level: "Grade 10" )
-
- grade_11 = Classroom.create(level: "Grade 11" )
-
- grade_12 = Classroom.create(level: "Grade 12" )
-
- grade_13 = Classroom.create(level: "Grade 13" )
