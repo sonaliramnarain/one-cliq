81dbb8cdd0774e644b592ae64f83a492ae51e8e3
sonali ramnarain <sonaliramnarain01@gmail.com>
2021-11-24 22:02:00 +0400

    home update

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,24 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["To", "connect!"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const banner = document.getElementById('banner-typed-text');
+  if (banner) {
+    new Typed('#banner-typed-text', {
+      strings: ["To", "connect!"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+  
+  const eventsTitle = document.getElementById('event_index');
+  if (eventsTitle) {
+    new Typed('#event_index', {
+      strings: ["Events"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+
 }
 
 export { loadDynamicBannerText };
