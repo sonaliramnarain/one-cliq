2d3bdce840234061eb1f019220606d2e51800fc1
inna goordoyal <innagoordoyal@gmail.com>
2021-11-24 14:42:13 +0400

    backdone

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -10,14 +10,16 @@ class AttendancesController < ApplicationController
     @attendance.student = student
     @attendance.agenda = agenda
     if @attendance.save
-      redirect_to controller: 'students', action: 'check', anchor: "student-#{student.id}"
+      redirect_to check_agenda_students_path(agenda, anchor: "student-#{student.id}")
     end
   end
 
   def destroy
     @attendance = Attendance.find(params[:id])
+    student = @attendance.student
     if @attendance.destroy
-      redirect_to controller: 'students', action: 'check', agenda_id: @attendance.agenda.id, anchor: "student-#{@attendance.student.id}"
+      # redirect_to controller: 'students', action: 'check', agenda_id: @attendance.agenda.id, anchor: "student-#{@attendance.student.id}"
+      redirect_to check_agenda_students_path(@attendance.agenda, anchor: "student-#{student.id}"), notice: "Message send to #{student.parent.name}: 'Your child is missing from school!'"
     end
   end
  private
--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -2,11 +2,11 @@ class Agenda < ApplicationRecord
   belongs_to :teacher, class_name: "User", foreign_key: "user_id"
   belongs_to :classroom
   validates :start_time, :end_time, presence: true
-  # after_create :create_attendances
+   after_create :create_attendances
 
-  # def create_attendances
-  #   self.classroom.students do |student|
-  #     Attendance.create!(agenda: self, student: student)
-  #   end
-  # end
+  def create_attendances
+    self.classroom.students.each do |student|
+      Attendance.create!(agenda: self, student: student)
+    end
+  end
 end
